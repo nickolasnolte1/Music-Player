95a0c708fd94b68080e8e0125f806625dd0895a0
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2021-04-25 17:39:47 -0600

    Update linkedlist.py
    Agregado el timer de las canciones

--- linkedlist.py
+++ linkedlist.py
@@ -59,3 +59,42 @@ _mainThread= ""
 
 for key,value in songListDictionary.items():
     print(key +" : " + value.strip("\n"))
+      
+class SongTimer(Thread):
+    def __init__(self,event,_timerWoking,_timeElapsed,playList):
+        self._playList = playList
+        self._timerWoking = _timerWoking
+        self._timeElapsed = _timeElapsed
+        self._songupdated = True
+        Thread.__init__(self)
+        self.stopped = event
+
+    def startTimer(self):
+        self._timerWoking = True
+        self._timeElapsed = 0
+        
+    def pauseTimer(self):
+        self._timerWoking = False
+
+    def stopTimer(self):
+        self._timeElapsed = 0
+        self._timerWoking = False
+        self.stopped.set()
+    
+    def resumeTimer(self):
+        self._timerWoking = True
+
+    def resetTimer(self):
+        self._timeElapsed = 0
+
+    def run(self):
+        while not self.stopped.wait(1):
+            if(self._timerWoking==True):
+                try:
+                    if(self._songupdated):
+                        print(("Playing: "+ self._playList.headval.dataval).strip("\n"))
+                        self._songupdated = False
+                    self._timeElapsed += 1
+                except Exception as e:
+                    self.stopTimer()
+                    print("Play List Ended")
